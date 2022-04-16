dea1719890d16ae3854c00a60f274b2fc33e139c
Myles <bracken.myles@gmail.com>
2022-04-16 10:32:54 -0700

    further updates to experience section

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -20,7 +20,54 @@ const query = graphql`
   }
 `
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  const data = useStaticQuery(query)
+  const {
+    allStrapiJobs: { nodes: jobs },
+  } = data
+  const [value, setValue] = React.useState(0)
+  const { company, position, date, desc } = jobs[value]
+  console.log(company, position, date, desc)
+
+  return (
+    <section className="section jobs">
+      <Title title="experience" />
+      <div className="jobs-center">
+        {/* btn container */}
+        <div className="btn-continer">
+          {jobs.map((item, index) => {
+            return (
+              // ternary logic that says to add job-btn class to everything and then adds active-btn to current selected
+              <button
+                key={item.strapiId}
+                onClick={() => setValue(index)}
+                // onClick with change the company dynamically by invoking setValue through arrow function
+                className={`job-btn ${index === value && "active-btn"}`}
+              >
+                {item.company}
+              </button>
+            )
+          })}
+        </div>
+        {/* job info */}
+        <article className="job-info">
+          <h3>{position}</h3>
+          <h4>{company}</h4>
+          <p className="job-date">{date}</p>
+          {desc.map(item => {
+            return (
+              <div key={item.id} className="job-desc">
+                <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+                <p>{item.name}</p>
+              </div>
+            )
+          })}
+        </article>
+      </div>
+      <Link to="/about" className="btn center-btn">
+        more info
+      </Link>
+    </section>
+  )
 }
 
 export default Jobs
